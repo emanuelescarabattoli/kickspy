62f819e45514c5973cb46ddfea115997a8f89ed8
Emanuele Scarabattoli <emanuele.scarabattoli@evonove.it>
2019-05-20 13:10:30 +0200

    Fixes

--- kickspy/kickspy/settings.py
+++ kickspy/kickspy/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6w!j_5%ex1k$w9aruo4=1o2kn0f6w!(zzka5scck4ad863*2wq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
